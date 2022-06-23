b60a8285244f4cd16660a1ca61a4b9f0da2b51a1
cenk.eralp <cenk.eralp@siemens.com>
2022-06-23 12:39:43 +0300

    Update download_libraries.sh

--- download_libraries.sh
+++ download_libraries.sh
@@ -3,5 +3,3 @@ stable = True # Set to True for latest pip version or False for main branch in G
 pip install {"tsai -U" if stable else "git+https://github.com/timeseriesAI/tsai.git"} >> /dev/null
 pip install sktime -U  >> /dev/null
 pip install catch22
-git clone https://github.com/CenkEralp/MiniRocket_Experiments.git
-cd MiniRocket_Experiments
